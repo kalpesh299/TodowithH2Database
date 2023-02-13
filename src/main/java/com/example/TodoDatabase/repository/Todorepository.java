79a03336938133c43c2d998dffdff42e61c68911
kalpesh patil <kalpesh123.kp78@gmail.com>
2023-02-14 00:55:13 +0530

    ..

--- src/main/java/com/example/TodoDatabase/repository/Todorepository.java
+++ src/main/java/com/example/TodoDatabase/repository/Todorepository.java
@@ -4,6 +4,7 @@ import com.example.TodoDatabase.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//
 @Repository
 public interface Todorepository extends JpaRepository<Todo,Integer> {///model ans data type of primary key here primary key is id so e ar using integer
 }
